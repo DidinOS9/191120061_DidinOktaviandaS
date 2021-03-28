e699e0eff0c89140d87e6a9b7378b36e77cd3454
DidinOS9 <didinoktaviandasetiawan@gmail.com>
2021-03-29 03:07:30 +0700

    belajar routing laravel

--- BelajarLaravel/routes/web.php
+++ BelajarLaravel/routes/web.php
@@ -14,3 +14,16 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello World !';
+});
+
+Route::get('/belajar', function () {
+    echo '<h1>Hello World</h1>';
+    echo '<p>Sedang belajar Laravel</p>';
+});
+
+Route::get('page/{nomor}', function($nomor){
+    return 'Ini Halaman ke-' . $nomor;
+});
